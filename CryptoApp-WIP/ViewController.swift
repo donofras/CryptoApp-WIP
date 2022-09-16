f52570bb341ef6d882fe99ddf431adfd182447b0
Onofras Denis <onofras.denisd@gmail.com>
2022-09-16 09:58:50 +0300

    chor: Added all files.

---
+++ CryptoApp-WIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoApp-WIP
+//
+//  Created by Denis Onofras on 16.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
